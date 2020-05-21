af8e7ec9d480a793fc3519c4f26e34bb1c67bc7d
Luis <nettalkangola@yahoo.com>
2020-05-20 20:48:30 -0400

    completed

---
+++ todo/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import TodoApp from "./components/TodoApp";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1> Todo App </h1>
+      <TodoApp />
+    </div>
+  );
+};
+
+export default App;
